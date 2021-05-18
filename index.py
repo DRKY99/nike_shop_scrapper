521ad8ed61f837ec6d3420865764eb9eb1744bd4
Ariel Lara <drkyofficial@gmail.com>
2021-05-18 00:45:21 -0500

    Menu 0.1

--- index.py
+++ index.py
@@ -4,39 +4,59 @@ import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
+#default path to chrome driver
+BROWSER = "Chrome"
+BROWSER_LIST =["Chrome","Firefox","Edge","Opera"]
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-
+URL = "http://nike.com"
+DELAY = 3
+DELAY_LIST=range(10)
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                    alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
+        #browser
+        self.browser_selector = QtWidgets.QComboBox()
+        self.browser_selector.addItems(BROWSER_LIST)
+        #driver path
+        self.driver_path = QtWidgets.QLineEdit(PATH)
+        #URL
+        self.url_path = QtWidgets.QLineEdit(URL)
+        #Delay
+        self.delay_selector = QtWidgets.QComboBox()
+        self.delay_selector.addItems([str(x) for x in DELAY_LIST])
+        #run script
+        self.run = QtWidgets.QPushButton("Run script")
+        #build
+        self.build()
+    
+    def build(self):
+        self.layout = QtWidgets.QFormLayout(self)
+        #==== widgets
+        #browser
+        self.layout.addRow("Select your browser",self.browser_selector)
+        #driver path
+        self.layout.addRow("Select your driver path",self.driver_path)
+        #URL
+        self.layout.addRow("Set the URL",self.url_path)
+        #Delay
+        self.layout.addRow("Select your delay",self.delay_selector)
+        #run
+        self.layout.addRow(self.run)
+        #==== methods
+        self.run.clicked.connect(self.runScript)
 
     @QtCore.Slot()
-    def magic(self):
+    def runScript(self):
         browser = webdriver.Chrome(PATH)
-        browser.get("http://nike.com")
-        time.sleep(5)
+        browser.get(URL)
+        time.sleep(DELAY)
 
 if __name__ == "__main__":
-    
-    
-
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(400, 600)
+    widget.resize(400, 150)
     widget.show()
 
     sys.exit(app.exec_())
